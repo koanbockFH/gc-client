450801576abf6c71c324c5c87d8cb9c7588027e8
riepljess <riepljessica@gmail.com>
2020-11-23 10:45:14 +0000

    Home Teacher changes

--- app/src/main/java/com/example/attendencemonitor/HomeTeacher.java
+++ app/src/main/java/com/example/attendencemonitor/HomeTeacher.java
@@ -6,12 +6,15 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
@@ -23,72 +26,21 @@ import java.io.IOException;
 
 public class HomeTeacher extends AppCompatActivity {
 
-
-    SurfaceView surfaceView;
-    CameraSource cameraSource;
-    TextView textView;
-    BarcodeDetector barcodeDetector;
+    private Button openScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_teacher);
 
-        surfaceView = (SurfaceView) findViewById(R.id.camerapreview);
-        textView = (TextView) findViewById(R.id.textView);
+        openScanner = (Button) findViewById(R.id.btn_openScanner);
 
-        barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
-
-        cameraSource = new CameraSource.Builder(this, barcodeDetector).setRequestedPreviewSize(640,480).build();
-
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+        openScanner.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void surfaceCreated(@NonNull SurfaceHolder holder) {
-                if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-                    return;
-                }
-                try {
-                    cameraSource.start(holder);
-                }catch(IOException e){
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
-                cameraSource.stop();
+            public void onClick(View v) {
+                //startActivity(new Intent(HomeTeacher.this, IdCodeActivity.class));
             }
         });
-
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
-
-                if(qrCodes.size() !=0){
-                    textView.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Vibrator vibrator = (Vibrator)getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-                            vibrator.vibrate(1000);
-                            //TODO only successfull if QR Code of student
-                            textView.setText(qrCodes.valueAt(0).displayValue);
-                        }
-                    });
-                }
-            }
-        });
-
 
     }
 }
