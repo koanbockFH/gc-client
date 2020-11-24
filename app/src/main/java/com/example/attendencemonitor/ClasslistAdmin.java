060a03609d9d0cc180451f5ab1817ec8f0759628
larissablachfelder <44058453+larissablachfelder@users.noreply.github.com>
2020-11-24 18:00:19 +0000

    statistics and classlist ui

---
+++ app/src/main/java/com/example/attendencemonitor/ClasslistAdmin.java
@@ -0,0 +1,16 @@
+package com.example.attendencemonitor;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClasslistAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics_view_admin);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/attendencemonitor/StatisticsViewAdmin.java
@@ -0,0 +1,16 @@
+package com.example.attendencemonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatisticsViewAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics_view_admin);
+
+        //TODO: handle all statistics xml here or make new activities for each
+    }
+}
